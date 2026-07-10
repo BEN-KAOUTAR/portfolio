34bdb9ebb2221ad8f8ad7673be021588b4402455
BEN-KAOUTAR <benhettouchkaoutar@gmail.com>
2026-07-10 18:10:28 +0100

    Copy CV PDF to build root assets during deployment

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kaoutar Ben-Hettouch | Portfolio',
+      title: ' Portfolio',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
